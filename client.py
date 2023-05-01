18fd0da9cf69feb15031de9b0b90f93378e03f8c
watermoon7 <111786878+watermoon7@users.noreply.github.com>
2023-05-01 03:03:14 +0100

    Update client.py
    made the code more stable
    added a better login
    added changes to the disconnect code

--- client.py
+++ client.py
@@ -1,15 +1,86 @@
-import socket, threading, utils, sys, time
-from tkinter import *
+import socket, threading, utils, sys, time, re
 from tkinter import messagebox
+from tkinter import *
 
-PORT = 5055
-DC_MESSAGE = '/dc'
+msg_list = ''
+PORT = 5056
 SERVER = '127.0.0.1'
-ADDRESS = (SERVER, PORT)
+NAME = ''
+failed_connections = 0
+client = None
+STATE = 'CLOSED'
+
+def connect():
+    global client, STATE
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((SERVER, PORT))
+    utils.send(client, NAME)
+    STATE = 'CONNECTED'
+
+def check_details():
+    global NAME, SERVER, PORT, failed_connections
+    
+    NAME = name_entry.get() if name_entry.get() not in [None, ""] and len(name_entry.get()) < 24 else None
+    SERVER = ip_entry.get() if re.match(r"^(\d{1,3}\.){3}\d{1,3}$", ip_entry.get()) else None
+    PORT = int(port_entry.get()) if port_entry.get().isdigit() else None
+    
+    if all(i != None for i in [NAME, SERVER, PORT]):
+        try:
+            connect()
+            root.destroy()
+        except:
+            failed_connections += 1
+            error_label.config(text=f'Failed to connect {failed_connections} times')
+    else:
+        msg = []
+        if not NAME:
+            msg.append('Name too long or invalid name')
+        if not SERVER:
+            msg.append('Invalid IP')
+        if not PORT:
+            msg.append('Invalid PORT')
+        
+        error_label.config(text=', '.join(msg))
+   
+# login details
+root = Tk()
+root.geometry("400x150")
+root.title("Chat Client")
+
+root.columnconfigure(1, weight=1)
+
+name_label = Label(root, text='Name')
+name_label.grid(row=0, column=0)
+name_entry = Entry(root)
+name_entry.grid(row=0, column=1, sticky='ew')
+
+ip_label = Label(root, text='IP')
+ip_label.grid(row=1, column=0)
+ip_entry = Entry(root)
+ip_entry.grid(row=1, column=1, sticky='ew')
+ip_entry.insert(0, '127.0.0.1')
+
+port_label = Label(root, text='Port')
+port_label.grid(row=2, column=0)
+port_entry = Entry(root)
+port_entry.grid(row=2, column=1, sticky='ew')
+port_entry.insert(0, '5056')
+
+error_label = Label(root, text='')
+error_label.grid(row=3, column=1)
+
+name_button = Button(root, text='Submit', command=check_details)
+name_button.grid(column=1)
+
+root.mainloop()
+# end of login
+
+DC_MESSAGE = '/dc'
 
 
 def update_chat():
-    global textbox, msg_list
+    global msg_list
+    
     textbox.config(state=NORMAL)
     textbox.delete('1.0', END)
     textbox.insert('1.0', msg_list)
@@ -18,7 +89,7 @@ def update_chat():
 
     
 def get_text(event=None):
-    global entry, msg_list
+    global msg_list
 
     new = entry.get()
     if new != '': 
@@ -27,56 +98,33 @@ def get_text(event=None):
 
     
 def recieving_feed(client):
-    global msg_list
-    time.sleep(0.4)
-
-    while True:
+    global msg_list, STATE
+    
+    recieving = True
+    while recieving:
         msg_list = utils.recieve(client)
-        update_chat()
         
-        if msg_list == None:
+        if msg_list in [DC_MESSAGE, None]:
             msg_list = "You have been disconnected. Restart the program to rejoin."
-            break
-
-        
-name = ''
-def check_name():
-    global name
-    if name_entry.get() not in [None, ""] and len(name_entry.get()) < 24:
-        name = name_entry.get()
-        initial.destroy()
+            root.unbind("<Return>")
+            button["state"] = DISABLED
+            
+            STATE = 'CLOSED'
+            recieving = False
+            
+        update_chat()
 
         
 def on_closing():
     if messagebox.askokcancel("Quit", "Do you want to quit?"):
         root.destroy()
-        try:
+        if STATE == 'CONNECTED':
             utils.send(client, '/dc')
-        except:
-            ...
+
+
             
 
-initial = Tk()
-initial.geometry("300x70")
-initial.title("Name Please")
-
-name_label = Label(initial, text='Name must be less than 24 characters')
-name_label.pack(side=BOTTOM, pady=5)
-name_entry = Entry(initial)
-name_entry.pack(side=LEFT, padx=10, pady=5)
-name_button = Button(initial, text='Submit', command=check_name)
-name_button.pack(side=LEFT)
-
-initial.mainloop()
-
-msg_list = ''
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDRESS)
-utils.send(client, name)
-
-threading.Thread(target=recieving_feed, args=[client]).start()
-
+# main loop
 root = Tk()
 root.title("Chat Client")
 root.geometry("600x450")
@@ -94,5 +142,6 @@ textbox.pack(padx=10, pady=5)
 
 root.protocol("WM_DELETE_WINDOW", on_closing)
 root.bind("<Return>",get_text)
-root.mainloop()
 
+threading.Thread(target=recieving_feed, args=[client]).start()
+root.mainloop()
